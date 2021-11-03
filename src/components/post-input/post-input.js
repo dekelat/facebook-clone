bf0218e1abcd9398bb5794ef66c847d45c706fd7
Dekel Atzami <Dekelat@elad.co.il>
2021-11-03 20:04:18 +0200

    input

--- src/components/post-input/post-input.js
+++ src/components/post-input/post-input.js
@@ -1,8 +1,19 @@
+import React, { useState } from 'react';
+
 export default function PostInput() {
+    const [post, setPost] = useState("");
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+        console.log(post);
+        setPost("");
+    };
+
     return (
         <div>
-            <form>
-                post input woorksss 
+            <form onSubmit={onSubmit}>
+               <input type="text" onChange={(event) => setPost(event.target.value)}/>
+               <button type="submit">post</button> 
             </form>
         </div>
     )
