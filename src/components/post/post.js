b9a0ec084147883c70919b6ba3a3c875d3ba3803
barzilaysapir <barzilaysapir@gmail.com>
2021-11-03 21:28:28 +0200

    started all posts style

--- src/components/post/post.js
+++ src/components/post/post.js
@@ -1,10 +1,30 @@
 import './post.scss';
 
-export default function Post() {
+export default function Post({ post }) {
 
     return (
         <div className="post">
-            postimmm
+            <div className="post-head">
+                <span className="name">
+                    {post.name}
+                </span>
+                <span className="date">
+                    {post.time.split("T")[0]}
+                </span>
+            </div>
+
+            <h2 className="post-body">
+                {post.message}
+            </h2>
+
+            <div className="likes">
+            <span>
+                {post.likes.length} likes
+            </span>
+                <button type="button">❤</button>
+            </div>
+
+            <input type="text" placeholder="comment here"></input>
         </div>
     );
 }
